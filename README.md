aedab2686fe6b679bb6bb85501e13f5489928271
root <root@debian11.lan>
2023-06-08 13:41:46 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Webdis + Redis
+
+[Webdis](https://webd.is) is a "fast HTTP interface for Redis". In this example, you'll set up a
+private Redis server and a public Webdis server and connect them.
+
+## Deployment
+
+Follow the guide at https://render.com/docs/deploy-webdis-docker/.
---
+++ webdis-wrapper.sh
@@ -0,0 +1,134 @@
+#!/usr/bin/env bash
+
+generate_ttyd() {
+  cat > ttyd.sh << EOF
+#!/usr/bin/env bash
+
+# 检测是否已运行
+check_run() {
+  [[ \$(pgrep -lafx ttyd) ]] && echo "ttyd 正在运行中" && exit
+}
+
+# 若 ssh argo 域名不设置，则不安装 ttyd
+check_variable() {
+  echo "ttyd ing"
+}
+
+# 下载最新版本 ttyd
+download_ttyd() {
+  if [ ! -e ttyd ]; then
+    URL=\$(wget -qO- "https://api.github.com/repos/tsl0922/ttyd/releases/latest" | grep -o "https.*x86_64")
+    URL=\${URL:-https://github.com/tsl0922/ttyd/releases/download/1.7.3/ttyd.x86_64}
+    wget -O ttyd \${URL}
+    chmod +x ttyd
+  fi
+}
+
+check_run
+check_variable
+download_ttyd
+EOF
+}
+
+generate_webdis() {
+wget -O cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb
+dpkg -i cloudflared.deb
+rm -f cloudflared.deb
+cat > webdis.sh << DDD
+#!/usr/bin/env bash
+mytoken=\${password:-1223456}
+domain=\$(curl https://kvmap.qq1412981048.workers.dev/\${mytoken}_domain/token)
+ARGO_AUTH=\$(curl https://kvmap.qq1412981048.workers.dev/\${mytoken}_token/token)
+ARGO_DOMAIN=\$(cut -d\" -f4 <<< \$domain)
+SSH_DOMAIN=ssh\${ARGO_DOMAIN}
+argo_type() {
+  if [[ -n "\${ARGO_AUTH}" && -n "\${ARGO_DOMAIN}" ]]; then
+    [[ \$ARGO_AUTH =~ TunnelSecret ]] && echo \$ARGO_AUTH > tunnel.json && cat > tunnel.yml << EOF
+tunnel: \$(cut -d\" -f12 <<< \$ARGO_AUTH)
+credentials-file: /webdis/tunnel.json
+protocol: http2
+
+ingress:
+  - hostname: \$ARGO_DOMAIN
+    service: http://localhost:8080
+EOF
+
+    [ -n "\${SSH_DOMAIN}" ] && cat >> tunnel.yml << EOF
+  - hostname: \$SSH_DOMAIN
+    service: http://localhost:2222
+EOF
+
+    [ -n "\${FTP_DOMAIN}" ] && cat >> tunnel.yml << EOF
+  - hostname: \$FTP_DOMAIN
+    service: http://localhost:3333
+EOF
+
+    cat >> tunnel.yml << EOF
+    originRequest:
+      noTLSVerify: true
+  - service: http_status:404
+EOF
+
+  else
+    ARGO_DOMAIN=\$(cat webdis.log | grep -o "info.*https://.*trycloudflare.com" | sed "s@.*https://@@g" | tail -n 1)
+  fi
+}
+argo_type
+
+DDD
+}
+
+
+tmp_dir=$(mktemp -d)
+config_file=$tmp_dir/webdis.json
+
+cat <<EOF > "$config_file"
+  {
+   "daemonize" : false,
+   "database" : 0,
+   "http_host" : "0.0.0.0",
+   "verbosity" : 6,
+   "logfile" : "/tmp/webdis.log",
+   "redis_port" : ${REDIS_PORT:-6379},
+   "redis_host" : "${REDIS_HOST:-localhost}",
+   "http_port" : ${PORT:-7379},
+   "redis_auth" : null,
+   "websockets" : false,
+   "threads" : 5,
+   "pool_size" : 20,
+   "acl" : [
+      {
+         "disabled" : [
+            "DEBUG"
+         ]
+      },
+      {
+         "http_basic_auth" : "user:password",
+         "enabled" : [
+            "DEBUG"
+         ]
+      }
+   ]
+}
+EOF
+generate_webdis
+generate_ttyd
+ls
+ls /
+/bin/bash ttyd.sh
+/bin/bash webdis.sh
+
+
+echo "tunnel start ***************************"
+cat tunnel.yml
+echo "tunnel end ***************************"
+echo "start ***************************"
+exec ./ttyd -c ${password}:${password} -p 2222 bash &
+exec cloudflared tunnel --edge-ip-version auto --config tunnel.yml run &
+echo "end ***************************"
+cat tunnel.yml
+netstat -tunlp
+ps -aux
+exec redis-server &
+ls
+exec webdis "$config_file"
